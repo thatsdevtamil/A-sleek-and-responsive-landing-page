67839c9bde27214093a1ef484207560a67199793
thatsdevtamil <thatsdevtamil@gmail.com>
2025-06-25 05:52:40 +0530

    Background Gradient Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
-    <>
-     <Navbar/>
-     <Hero/>
-    </>
-  )
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
